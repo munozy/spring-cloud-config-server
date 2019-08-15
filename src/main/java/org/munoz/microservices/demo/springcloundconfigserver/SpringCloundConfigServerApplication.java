f77da2df16613cf2c4b9d11370209a78638c851d
Yupanqui Munoz <yupanqui@gmail.com>
2019-08-15 11:58:30 +0200

    first commit

---
+++ src/main/java/org/munoz/microservices/demo/springcloundconfigserver/SpringCloundConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.munoz.microservices.demo.springcloundconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloundConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloundConfigServerApplication.class, args);
+	}
+
+}
